311e7d6ebda350b7c49d52df504e5a241205c9ff
Islam Lurjie <ramzislam125@gmail.com>
2026-01-23 23:21:08 +0300

    version_1.4

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -24,7 +24,11 @@ comments_patterns = [
 ]
 
 urlpatterns = [
-    path('v1/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'v1/jwt/create/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
     path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, filters, mixins
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (
     IsAuthenticated,
-    IsAuthenticatedOrReadOnly, 
+    IsAuthenticatedOrReadOnly,
     AllowAny
 )
 from django.shortcuts import get_object_or_404
@@ -22,6 +22,7 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
     pagination_class = LimitOffsetPagination
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -29,6 +30,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    
     def get_queryset(self):
         post_pk = self.kwargs.get("post_pk")
         post = get_object_or_404(Post, pk=post_pk)
